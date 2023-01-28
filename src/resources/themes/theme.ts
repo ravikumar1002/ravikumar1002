ad634659c42dbe812dc97c7e459f056618a51bd8
Ravi kumar <1002kumarravi@gmail.com>
2023-01-28 17:58:22 +0530

    fix ts

--- src/resources/themes/theme.ts
+++ src/resources/themes/theme.ts
@@ -1,11 +1,9 @@
-import { createTheme, Fade, PaletteMode, Theme, ThemeOptions } from '@mui/material';
+import { createTheme, alpha, PaletteMode, Theme, ThemeOptions } from '@mui/material';
 
 
-declare module '@mui/material/styles' {
-  interface palette {
-    mode: PaletteMode,
-  }
-}
+// declare module '@mui/material/styles' {
+
+// }
 
 
 const theme: ThemeOptions = createTheme({
@@ -20,39 +18,36 @@ const theme: ThemeOptions = createTheme({
     },
     background: {
       paper: "#0A192F"
-    },
-    offWhite: {
-      main: "rgb(136, 146, 176)"
     }
   },
-  props: {
-    MuiPaper: {
-      elevation: 0,
-      square: true
-    }
-  },
-  overrides: {
+  components: {
     MuiTypography: {
-      root: {
-        // color: "rgb(136, 146, 176)"
-        color: "rgb(187 187 187)"
+      styleOverrides: {
+        root: {
+          color: "rgb(187 187 187)"
+        }
+      }
+    },
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          elevation: 0,
+          square: true
+        }
       }
     },
     MuiButton: {
-      root: {
-        color: "#fff",
-        textTransform: "inherit",
-      },
-      textSizeSmall: {
-        fontSize: "0.76rem"
+      styleOverrides: {
+        root: {
+          color: alpha("#fff", 0.8),
+          textTransform: "inherit",
+        },
+        textSizeSmall: {
+          fontSize: "0.76rem"
+        }
       }
     }
   },
-  custom: {
-    fontFamily: {
-      codeStyle: `"SF Mono", "Fira Code", "Fira Mono", "Roboto Mono", "Lucida Console", Monaco, monospace`
-    }
-  }
 })
 
 export default theme
