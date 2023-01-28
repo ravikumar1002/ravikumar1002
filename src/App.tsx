8a19632b2c69f3a0ade52e6c732bc16e525d317e
Ravi kumar <1002kumarravi@gmail.com>
2023-01-28 18:00:33 +0530

    main screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,49 @@
-
+import makeSxStyles from "@hooks/makeSxStyles";
 import "./App.css";
 
-function App() {
+import { Box, Theme, useTheme } from "@mui/material";
+import About from "@components/About";
+import Contact from "@components/Contact";
+import Work from "@components/Work";
+import LandingPage from "@components/LandingPage";
+import Header from "@components/Header";
+import LeftSidebar from "@components/LeftSidebar";
+import RightSidebar from "@components/RightSidebar";
 
-  return <div className="App"></div>;
-}
+const useStyles = makeSxStyles((theme: Theme) => ({
+  pageContainer: {
+    backgroundColor: theme.palette.primary.main,
+    padding: theme.spacing(0, 22),
+    [theme.breakpoints.down("md")]: {
+      padding: theme.spacing(0, 20),
+    },
+    [theme.breakpoints.down("sm")]: {
+      padding: theme.spacing(0, 12),
+    },
+    [theme.breakpoints.down("xs")]: {
+      padding: theme.spacing(0, 4),
+    },
+  },
+}));
+
+const App = () => {
+  const theme = useTheme();
+  const classes = useStyles(theme);
+  return (
+    <div className="App">
+      <Box>
+        <Header />
+        <LeftSidebar />
+        <RightSidebar />
+        <Box sx={classes.pageContainer}>
+          <LandingPage />
+          <About />
+          <Work />
+          <Contact />
+        </Box>
+      </Box>
+    </div>
+  );
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { ThemeProvider } from "@mui/material";
+import theme from "@resources/themes";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
