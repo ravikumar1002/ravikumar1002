cd43278444bd928ec66cad0b9b646f3df85ccf18
Ravi kumar <1002kumarravi@gmail.com>
2023-01-29 10:59:36 +0530

    remove: extra code

--- src/hooks/makeSxStyles/makeSxStyles.tsx
+++ src/hooks/makeSxStyles/makeSxStyles.tsx
@@ -27,28 +27,9 @@ export type MakeSxStyleResponse<
   Props extends object,
   ClassKey extends string = string
 > = keyof Props extends never
-  ? // `MakeSxStyleResponse` where the passed `styles` do not depend on props
-    (props?: any) => ClassNameMap<Theme, ClassKey>
-  : // `MakeSxStyleResponse` where the passed `style s` do depend on props
-    (props: Props) => ClassNameMap<Theme, ClassKey>;
+  ? (props?: any) => ClassNameMap<Theme, ClassKey>
+  : (props: Props) => ClassNameMap<Theme, ClassKey>;
 
-/**
- * Create a React hooks to get the object sx styles. This is similar to the `makeStyles` on `@mui/styles`
- * @param styles - It can be a object with of type `Record<string,SxProps<Theme>>` or a function which accept the props a params.
- * Example 1 - simple object:
- * const useStyles = makeSxStyles({container: {color: 'primary.main'}});
- *
- * const classes = useStyles()
- * <Box sx={classes.container} />
- *
- * Example 2 - function:
- * const useStyles = makeSxStyles((props) => ({
- *  container: {color: props.isPrimary ? 'primary.main' : 'secondary.main'}
- * }));
- *
- * const classes = useStyles({isPrimary: true})
- * <Box sx={classes.container} />
- */
 const makeSxStyles =
   <
     Theme extends DefaultTheme,
