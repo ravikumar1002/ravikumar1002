eb995e4fdc8e154a1186237e7c7a4977bd15217b
Ravi kumar <1002kumarravi@gmail.com>
2023-01-31 15:26:12 +0530

    fix resume link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -193,7 +193,7 @@ const Header = () => {
               target="_blank"
               component="a"
               variant="outlined"
-              href="https://drive.google.com/file/d/1twX-1Oy_DyDWe9-UtnrhVXStPkAuuz9A/view?usp=share_link"
+              href="https://drive.google.com/file/d/1g92DMJOyQp3i0uGaU8DGr1_4WOClUm4s/view?usp=share_link"
               sx={{
                 fontSize: "1.1rem"
               }}
